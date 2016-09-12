e29f521aea4f01b0c278ad1b14630d3272183399
honnix <hxliang1982@gmail.com>
2016-09-12 09:55:49 +0200

    mount users.digest

--- start.sh
+++ start.sh
@@ -3,6 +3,7 @@
 docker run \
 -d \
 -v `pwd`/etc/cobbler/settings:/etc/cobbler/settings \
+-v `pwd`/etc/cobbler/users.digest:/etc/cobbler/users.digest \
 -v `pwd`/mnt:/mnt:ro \
 -v `pwd`/var/www/cobbler/images:/var/www/cobbler/images \
 -v `pwd`/var/www/cobbler/ks_mirror:/var/www/cobbler/ks_mirror \
